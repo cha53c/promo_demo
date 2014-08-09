13ed0cbed8c11cc2e3afc6a1cb0dee460f3adf9b
chrischase <chr15t0pher.cha53@gmail.com>
2014-08-09 23:34:08 +0200

    added TODOs

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,6 @@
 class Client < ActiveRecord::Base
   # this will map to promotion
   # validates :name, presence: true, length: {minimum:  1}
-
+  # TODO include validations
+  # TODO include default values
 end
