cec10b7048ed5135087978cc7a830002526c99f3
chrischase <chr15t0pher.cha53@gmail.com>
2014-08-10 22:54:11 +0200

    adding destroy to promotions

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ActiveRecord::Base
-  has_many :promotions
+  has_many :promotions, dependent: :destroy
   # this will map to promotion
   # validates :name, presence: true, length: {minimum:  1}
   # TODO include validations
