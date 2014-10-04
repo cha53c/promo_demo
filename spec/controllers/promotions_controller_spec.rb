fac636306d4fa57a919fd58b40a4f41215cc969d
chrischase <chr15t0pher.cha53@gmail.com>
2014-10-04 19:39:08 +0200

    refactored exampled

--- spec/controllers/promotions_controller_spec.rb
+++ spec/controllers/promotions_controller_spec.rb
@@ -4,7 +4,16 @@ require 'rails_helper'
 RSpec.describe PromotionsController do
   let(:client) { mock_model(Client) }
   let(:promotion) { mock_model(Promotion) }
-  let(:params) {{client_id: client, id: promotion.id, promotion: {:promotion => {:promo_type => "promo1", :client_id => client.id}}}}
+  let(:params) { {client_id: client, id: promotion.id,
+                  promotion: {promotion: {promo_type: "promo1"}}} }
+
+  before {
+    allow(Client).to receive(:find).and_return(client)
+    allow(client).to receive(:promotions).and_return(promotion)
+    allow(promotion).to receive(:create).and_return(promotion)
+    allow(promotion).to receive(:find).and_return(promotion)
+    allow(Promotion).to receive(:find)
+  }
 
   describe 'GET new' do
     subject(:get_new) { get :new, :client_id => client.id }
@@ -14,12 +23,7 @@ RSpec.describe PromotionsController do
   end
 
   describe 'POST create' do
-    before {
-      allow(Client).to receive(:find).and_return(client)
-      allow(client).to receive(:promotions).and_return(promotion)
-      allow(promotion).to receive(:create).and_return(promotion)
-    }
-    subject(:post_create) { post :create, params}
+    subject(:post_create) { post :create, params }
 
     it 'finds the client' do
       expect(Client).to receive(:find).once()
@@ -36,15 +40,10 @@ RSpec.describe PromotionsController do
 
   describe 'PATCH update' do
     before {
-      allow(Client).to receive(:find).and_return(client)
-      allow(client).to receive(:promotions).and_return(promotion)
-      allow(promotion).to receive(:create).and_return(promotion)
       expect(Promotion).to receive(:find).and_return(promotion)
       expect(Client).to receive(:find).and_return(client)
     }
     subject(:patch_update) { patch :update, params }
-    # it 'finds the promotion'
-    # it 'finds the client'
     it 'calls update on the promotion' do
       expect(promotion).to receive(:update)
       patch_update
@@ -70,14 +69,8 @@ RSpec.describe PromotionsController do
   end
 
   describe "GET destroy" do
-    before {
-      allow(Client).to receive(:find).and_return(client)
-      allow(client).to receive(:promotions).and_return(promotion)
-      allow(promotion).to receive(:find).and_return(promotion)
-      allow(promotion).to receive(:destroy).and_return(promotion)
-    }
+
     subject(:delete_destroy) { delete :destroy, params }
-    # it 'finds the promotion'
     it 'destroys the promotion' do
       expect(promotion).to receive(:destroy)
       delete_destroy
@@ -88,14 +81,12 @@ RSpec.describe PromotionsController do
   end
 
   describe "GET edit" do
-    subject(:get_edit) { get :edit, params}
+    subject(:get_edit) { get :edit, params }
     it "finds promotion" do
-      allow(Client).to receive(:find)
       expect(Promotion).to receive(:find).and_return(promotion)
       get_edit
     end
     it "finds client" do
-      allow(Promotion).to receive(:find)
       expect(Client).to receive(:find).and_return(client)
       get_edit
     end
