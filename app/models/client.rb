0d723103fc83a3ecfe0a489507ffb39a9a91dec6
chrischase <chr15t0pher.cha53@gmail.com>
2014-10-27 19:58:20 +0100

    added field with erros

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < ActiveRecord::Base
   has_many :promotions, dependent: :destroy
-  validates :photo, :tel, :website, :email, presence: true
+  validates :photo, :tel, :website, :email, :name, presence: true
   # TODO include validations, for image size
   has_attached_file :photo, :styles => {:medium => "400x400>", :small => "200x200>"}
   validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
