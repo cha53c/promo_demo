3afb9544f82cfd2c583c2d57fb09b44ad482c543
chrischase <chr15t0pher.cha53@gmail.com>
2015-05-08 19:28:00 +0200

    add client data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,15 +32,15 @@ address = Address.create!(line1: '101A Mandela House', country: 'The Plaza', tow
 #                photo: Rack::Test::UploadedFile.new(Rails.root + 'spec/fixtures/images/test_image.jpg', 'image/jpg'),
 #                email: 'client1@test.com', tel: '0890775544', website: 'http://www.client1.com')
 
-# patagonia = Client.create!(name: 'Patagonia',
-#                            photo: Rack::Test::UploadedFile.new(Rails.root + 'spec/fixtures/images/clients/patagonia.jpg', 'image/jpg'),
-#                            email: 'patagonia@test.com', tel: '0890775544', website: 'http://www.patagonia.com')
-#
-# Promotion.create!(promo_type: "10% off", description: "Winter Offer", details: "10% off total bill every tuesday",
-#                   client_id: patagonia.id, cuisine_id: argentinian.id, theme_id: meal_deals,
-#                   image: Rack::Test::UploadedFile.new(Rails.root + 'spec/fixtures/images/promotions/steak.jpg', 'image/jpg'),
-#                   starts: Date.today.strftime('%d-%m-%Y'), ends: (Date.today + 90).strftime('%d-%m-%Y'), tue: '1')
-#
+patagonia = Client.create!(name: 'Patagonia',
+                           photo: Rack::Test::UploadedFile.new(Rails.root + 'spec/fixtures/images/clients/patagonia.jpg', 'image/jpg'),
+                           email: 'patagonia@test.com', tel: '0890775544', website: 'http://www.patagonia.com')
+
+Promotion.create!(promo_type: "10% off", description: "Winter Offer", details: "10% off total bill every tuesday",
+                  client_id: patagonia.id, cuisine_id: argentinian.id, theme_id: meal_deals,
+                  image: Rack::Test::UploadedFile.new(Rails.root + 'spec/fixtures/images/promotions/steak.jpg', 'image/jpg'),
+                  starts: Date.today.strftime('%d-%m-%Y'), ends: (Date.today + 90).strftime('%d-%m-%Y'), tue: '1')
+
 # jbs = Client.create!(name: 'JBs Cafe',
 #                            photo: Rack::Test::UploadedFile.new(Rails.root + 'spec/fixtures/images/clients/jbs.jpg', 'image/jpg'),
 #                            email: 'jbs@test.com', tel: '0890775544', website: 'http://www.jbs.com')
