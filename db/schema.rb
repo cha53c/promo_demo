0f7e9b32d10121b584c8f43ac0d8f4872c7400f6
chrischase <chr15t0pher.cha53@gmail.com>
2015-08-03 16:52:46 +0200

    add name field to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730172441) do
+ActiveRecord::Schema.define(version: 20150803144346) do
 
   create_table "addresses", force: true do |t|
     t.string   "line1"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20150730172441) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "name"
   end
 
 end
