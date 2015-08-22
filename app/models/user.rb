4974c71f7a162f47757f53d4efc1a8c32243cef3
chrischase <chr15t0pher.cha53@gmail.com>
2015-08-22 14:03:42 +0200

    add scope for users that have never signed in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   scope :unconfirmed, -> {where(confirmed_at: nil)}
+  scope :nosignin, -> {where(sign_in_count: 0)}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
