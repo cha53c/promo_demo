c01189e02bd26af05bc8c5f5bdf482edb38a81c8
chrischase <chr15t0pher.cha53@gmail.com>
2015-08-26 19:42:00 +0200

    removed crud operations from UsersController, not handled by Devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
-  # TODO CUD actions will be handled by devise and should be removed
-
-  # This will handle actions for desplaying and searching user.
+  # This will handle actions for displaying and searching user.
   # all account creation and updates and destroys are handled by devise
   after_filter :verify_authorized
 
@@ -10,60 +8,9 @@ class UsersController < ApplicationController
     authorize @users
   end
 
-  # new shows the page with the fields to create the user
-  # note the user is not created by this method, that is done by create
-  def new
-     @user = User.new
-     authorize @user
-   end
-  # create actually create the user in the database, using the data received from the form
-  def create
-    @user = User.new(user_params)
-    authorize @user
-    if @user.save
-      # TODO we need to make sure that devise logs and appropriate trail for audit purposes
-      logger.info("user_id: #{current_user.id} create new user: #{@user.email}")
-      flash.now.notice="New user: #{@user.email} created"
-      render 'show'
-    else
-      logger.warn("user_id: #{current_user.id} failed to create new user: #{@user.email}")
-      flash.now.alert="New user: #{@user.email} could not be created"
-      render "new"
-    end
-  end
-
-  def destroy
-    @user = find_by_id(params[:id])
-    authorize @user
-    @user.destroy
-    logger.info("user_id: #{current_user.id} deleted user #{@user.email}")
-    redirect_to users_path, notice: "User #{@user.email} has been deleted"
-  end
-
   def show
     @user = find_by_id(params[:id])
     authorize @user
-  end
-
-  def edit
-    @user = find_by_id(params[:id])
-    authorize @user
-  end
-
-  def update
-    # TODO ask for confirmation before updating records
-    @user = find_by_id(params[:id])
-    authorize @user
-    org_user_email = @user.email
-    if @user.update(user_params)
-      logger.info("user_id #{current_user.id} updated details for user #{org_user_email}")
-      flash.now.notice="User #{@user.email} details updated"
-      render 'show'
-    else
-      logger.warn("user_id #{current_user.id} attempted to update details for user #{org_user_email}, but update failed")
-      flash.now.alert="Failed to update details for User #{org_user_email}"
-      render 'edit'
-    end
   end
 
   private
@@ -72,6 +19,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
+    # TODO remove the params that are not need now as they are handled by Devise
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :role)
   end
 end
