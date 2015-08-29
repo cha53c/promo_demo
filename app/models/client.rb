98af1faec20aecdcc48e57d2419226a50b9619af
chrischase <chr15t0pher.cha53@gmail.com>
2015-08-29 16:19:46 +0200

    remove present restrictions on fields

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,7 +3,6 @@ class Client < ActiveRecord::Base
   has_many :users
   has_one :address
   accepts_nested_attributes_for :address, allow_destroy: true
-  validates :photo, :tel, :website, :email, :name, presence: true
   validates_uniqueness_of :name
   has_attached_file :photo, :styles => {:medium => "400x400>", :small => "200x200>"}
   validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
@@ -11,5 +10,5 @@ class Client < ActiveRecord::Base
   validates_length_of :name, maximum: 20
   validates_length_of :email, maximum: 50
   validates_length_of :website, maximum: 50
-  validates_format_of :email, with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+  validates_format_of :email, with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/, allow_blank: true
 end
