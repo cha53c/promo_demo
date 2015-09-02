f88965308d98b0c03f4102f5529a0f7ebc2ce7d8
chrischase <chr15t0pher.cha53@gmail.com>
2015-09-02 22:13:27 +0200

    remove validation checks

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -2,7 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Address, :type => :model do
 
-  it { should validate_presence_of(:line1) }
-  it { should validate_presence_of(:country) }
   it { should belong_to(:client)}
 end
