f9f881d5566389ae725d44f5b20326c91bd68b68
chrischase <chr15t0pher.cha53@gmail.com>
2015-09-03 10:19:51 +0200

    remove new and create, these are now handled by devise

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,22 +11,4 @@ RSpec.describe UsersController, :type => :controller do
     session[:user_id] = user.id
   }
 
-  describe "GET new" do
-    subject(:get_new) { get :new }
-    it "renders new" do
-      expect(get_new).to render_template('users/new')
-    end
-  end
-
-  describe "POST create" do
-    subject(:post_create) { post :create, user: attributes_for(:user) }
-
-    it 'call save on user' do
-      expect(User).to receive(:new).and_return(user)
-      expect(user).to receive(:save)
-      post_create
-    end
-    # context 'user save successfully'
-  end
-
 end
