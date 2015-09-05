b362d9e0138e221dafe950171950794a637b5241
chrischase <chr15t0pher.cha53@gmail.com>
2015-09-05 18:24:41 +0200

    add publish flag to client show

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -2,4 +2,12 @@ module ClientsHelper
   def find_client id
     @client = Client.find(id)
   end
+
+  def published_yes_no published
+    if published
+      "glyphicon glyphicon-ok text-success"
+    else
+      "glyphicon glyphicon-remove text-danger"
+    end
+  end
 end
