3b8376d795a3c1ed6ecd5544b9f093878f481499
chrischase <chr15t0pher.cha53@gmail.com>
2015-09-06 20:53:17 +0200

    user promotions index to render the promotions on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   helper :clients
   def index
-    @promotions = Promotion.all
+    @promotions = Promotion.published
+    render 'promotions/index'
+    # redirect_to promotions_path
   end
 end
