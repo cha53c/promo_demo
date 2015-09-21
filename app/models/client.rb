994a868df200d9975f401f46fd484966b700e968
chrischase <chr15t0pher.cha53@gmail.com>
2015-09-21 18:16:16 +0200

    removed content type validation to check cause of error on drone

--- app/models/client.rb
+++ app/models/client.rb
@@ -8,10 +8,11 @@ class Client < ActiveRecord::Base
 
   validates_uniqueness_of :name
   has_attached_file :photo, :styles => {:medium => "400x400>", :small => "200x200>"}
-  validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
+  # validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
   validates_attachment_size :photo, less_than: 1.megabyte
   validates_length_of :name, maximum: 20
   validates_length_of :email, maximum: 50
   validates_length_of :website, maximum: 50
   validates_format_of :email, with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/, allow_blank: true
 end
+v
