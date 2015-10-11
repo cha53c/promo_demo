6bb0d8bbee5c66d76f1b4847a24aff32bdd06556
chrischase <chr15t0pher.cha53@gmail.com>
2015-10-11 14:58:04 +0200

    use fixture_file_upload to wrap file upload

--- features/step_definitions/keyword_search_steps.rb
+++ features/step_definitions/keyword_search_steps.rb
@@ -1,9 +1,12 @@
 Given(/^The promotion exists$/) do
+  # client = Client.create!(name: "testclient", tel: "123", email: "me@home.com",
+  #                         website: "www.bookme.com",
+  #                         photo: Rack::Test::UploadedFile.new(Rails.root + 'spec/fixtures/images/promotions/test_image.jpg', 'image/jpg'))
   client = Client.create!(name: "testclient", tel: "123", email: "me@home.com",
                           website: "www.bookme.com",
-                          photo: Rack::Test::UploadedFile.new(Rails.root + 'spec/fixtures/images/promotions/test_image.jpg', 'image/jpg'))
+                          photo: fixture_file_upload("#{Rails.root}/spec/fixtures/images/promotions/test_image.jpg", 'image/jpg'))
   Promotion.create!(client_id: client.id,
-                    image: Rack::Test::UploadedFile.new(Rails.root + 'spec/fixtures/images/promotions/test_image.jpg', 'image/jpg'),
+                    image: fixture_file_upload("#{Rails.root}/spec/fixtures/images/promotions/test_image.jpg", 'image/jpg'),
                     description: "Burger Madness",  promo_type: "2 for 1", wed: '1',
                     details: "Lorem ipsum dolor sit amet,",
                     starts: Date.today.strftime('%d-%m-%Y'), ends: Date.today.strftime('%d-%m-%Y'))
